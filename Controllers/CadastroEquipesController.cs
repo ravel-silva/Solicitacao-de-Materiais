ecf8369236f1ee74a7ac225344ffdcc4fdefd328
Ravel Mesquita <ravels78@gmail.com>
2024-12-21 21:21:15 -0300

    Configurando o Banco de Dados MySql

--- Controllers/CadastroEquipesController.cs
+++ Controllers/CadastroEquipesController.cs
@@ -9,6 +9,7 @@ namespace Solicitacao_de_Material.Controllers
     public class CadastroEquipesController : ControllerBase
     {
         private static List<CadastroEquipe> equipes = new List<CadastroEquipe>(); // This is a list of CadastroEquipe objects
+        // This method creates a team
         [HttpPost]
         public IActionResult CreateTeam([FromBody] CreateCadastroEquipeDto CadastroEquipeDto)
         {
@@ -19,11 +20,23 @@ namespace Solicitacao_de_Material.Controllers
             equipes.Add(equipe);
             return Ok();
         }
-
+        // This method returns the list of teams
         [HttpGet]
         public IActionResult GetTeams()
         {
             return Ok(equipes);
+        }
+        // This method deletes a team
+        [HttpDelete("{id}")]
+        public IActionResult DeleteTeam(int id)
+        {
+            CadastroEquipe equipe = equipes.FirstOrDefault(equipe => equipe.Id == id);
+            if (equipe == null)
+            {
+                return NotFound();
+            }
+            equipes.Remove(equipe);
+            return NoContent();
         }
     }
 }
---
+++ Data/EquipeContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Solicitacao_de_Material.Model;
+
+namespace Solicitacao_de_Material.Data
+{
+    public class EquipeContext : DbContext
+    {
+        public EquipeContext(DbContextOptions<EquipeContext> options) : base(options)
+        {
+        }
+        public DbSet<CadastroEquipe> Equipes { get; set; }
+    }
+}
